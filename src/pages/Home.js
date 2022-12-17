f1667efd62bed71dca23f220bc7473f2a8645b52
Somanyu <somanyu.03samal@gmail.com>
2022-12-17 18:54:43 +0530

    ♻️ changed class to className

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,64 +5,64 @@ const Home = () => {
         <div>
             <Navbar />
 
-            <div class="overflow-x-auto relative mt-8 p-7 font-inter">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="overflow-x-auto relative mt-28 p-7 font-inter">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="py-3 px-6">
+                            <th scope="col" className="py-3 px-6">
                                 Product name
                             </th>
-                            <th scope="col" class="py-3 px-6">
+                            <th scope="col" className="py-3 px-6">
                                 Color
                             </th>
-                            <th scope="col" class="py-3 px-6">
+                            <th scope="col" className="py-3 px-6">
                                 Category
                             </th>
-                            <th scope="col" class="py-3 px-6">
+                            <th scope="col" className="py-3 px-6">
                                 Price
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Apple MacBook Pro 17"
                             </th>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 Sliver
                             </td>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 Laptop
                             </td>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 $2999
                             </td>
                         </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Microsoft Surface Pro
                             </th>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 White
                             </td>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 Laptop PC
                             </td>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 $1999
                             </td>
                         </tr>
-                        <tr class="bg-white dark:bg-gray-800">
-                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr className="bg-white dark:bg-gray-800">
+                            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Magic Mouse 2
                             </th>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 Black
                             </td>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 Accessories
                             </td>
-                            <td class="py-4 px-6">
+                            <td className="py-4 px-6">
                                 $99
                             </td>
                         </tr>
