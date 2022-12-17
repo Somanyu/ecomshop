17f06631cb83e44de097763f8662ebf8c356ace8
Somanyu <somanyu.03samal@gmail.com>
2022-12-17 22:23:08 +0530

    🔧 add: just-in-time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
