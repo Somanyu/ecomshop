5a239aa90ab415db3ec3361ce187d4919b7d6a50
Somanyu <somanyu.03samal@gmail.com>
2022-12-17 13:46:33 +0530

    💄 update: extended fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', ...defaultTheme.fontFamily.sans],
+        karla: ['Karla', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
